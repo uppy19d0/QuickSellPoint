b7e0535f84e1f7a94dfefd2068d2bdbb3d8e2326
Luis Aneuris Tavarez <luis_aneuris16@hotmail.com>
2021-03-03 18:45:49 -0400

    administracion de proyecto

---
+++ Aplicacion1JavaFX/src/module-info.java
@@ -0,0 +1,9 @@
+module Aplicacion1JavaFX {
+    requires javafx.base;
+    requires  javafx.controls;
+    requires  javafx.fxml;
+    requires java.desktop;
+    requires java.sql;
+    requires commons.codec;
+    opens screensframework;
+}
---
+++ Aplicacion1JavaFX/src/test/UnitTesting.java
@@ -0,0 +1,11 @@
+//package test;
+
+//import screensframework.ProductoController;
+//import org.junit.Test;
+//import org.junit.Assert;
+//public class UnitTesting{
+
+  //  ProductoController producto;
+
+
+//}
